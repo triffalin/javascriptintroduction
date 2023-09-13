6816581cc8213df68606ff50b2030e90151c5fa3
Alin Triff <triffalin@gmail.com>
2023-09-13 15:39:23 +0300

    outsourced js code into external file

---
+++ app.js
@@ -0,0 +1,8 @@
+let age = 32;
+let greetingText = "Hi, I'm Alin!";
+alert(greetingText);
+alert(greetingText);
+greetingText = 'Hi, I am really Alin';
+alert(greetingText);
+alert(age);
+alert(age);
