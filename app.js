c9c25c96269b3c8de82756accd3c6c60246adc64
Alin Triff <triffalin@gmail.com>
2023-09-13 18:30:20 +0300

    splitted code across multiples lines

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let age = 32;
 let userName = 'Alin';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
-let job = { title: 'Developer', place: 'New York', salary: 5000 };
+let job = {
+	title: 'Developer',
+	place: 'New York',
+	salary: 5000,
+};
 
 alert(hobbies[0]);
+alert(job.title);
