db64fe3411db19fa2ea18321b0830a011470d37f
Alin Triff <triffalin@gmail.com>
2023-09-14 15:16:12 +0300

    introduced the modulus operator

--- operations.js
+++ operations.js
@@ -4,3 +4,6 @@ console.log(10 + 4);
 console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
+
+console.log(10 % 4); // 10/4 => 2 * 4 => Remainder: 2
+console.log(10 % 3); // Expected Remainder: 1
