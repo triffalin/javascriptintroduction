37a03f1ad19f9eb2fedae0e52ec09dd29f39b384
Alin Triff <triffalin@gmail.com>
2023-09-14 15:25:33 +0300

    tested the math operations and math rules

--- operations.js
+++ operations.js
@@ -7,3 +7,20 @@ console.log(10 / 4);
 
 console.log(10 % 4); // 10/4 => 2 * 4 => Remainder: 2
 console.log(10 % 3); // Expected Remainder: 1
+
+console.log(10 + 3 - 5 * 10); // expected: -37
+console.log((10 + 3 - 5) * 10); // changed the priority, expected result: 80
+
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result = result + 1;
+console.log(result);
+
+// Shorthand
+result++; // result = result + 1
+result--; // result = result - 1
+
+result += 5; // result = result + 5
+result -= 5; // result = result - 5
+result *= 5; // result = result * 5
+result /= 5; // result = result / 5
